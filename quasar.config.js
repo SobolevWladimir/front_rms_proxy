18ac543e84896d2cca37ce0b85a31166c73ed41b
Vladimir <Sobolev.Wladim@yandex.ru>
2025-01-28 16:18:31 +0300

    Набросал примерный вид

--- quasar.config.js
+++ quasar.config.js
@@ -37,7 +37,7 @@ export default defineConfig((/* ctx */) => {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#build
     build: {
       target: {
-        browser: [ 'es2022', 'firefox115', 'chrome115', 'safari14' ],
+        browser: ['es2022', 'firefox115', 'chrome115', 'safari14'],
         node: 'node20'
       },
 
@@ -59,7 +59,7 @@ export default defineConfig((/* ctx */) => {
 
       // extendViteConf (viteConf) {},
       // viteVuePluginOptions: {},
-      
+
       vitePlugins: [
         ['vite-plugin-checker', {
           eslint: {
@@ -73,7 +73,13 @@ export default defineConfig((/* ctx */) => {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#devserver
     devServer: {
       // https: true,
-      open: true // opens browser window automatically
+      open: true, // opens browser window automatically
+       '/ws': {
+        target: 'localhost:9090/ws',
+        ws: true,
+        rewriteWsOrigin: true,
+      },
+
     },
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#framework
@@ -114,7 +120,7 @@ export default defineConfig((/* ctx */) => {
     // https://v2.quasar.dev/quasar-cli-vite/developing-ssr/configuring-ssr
     ssr: {
       prodPort: 3000, // The default port that the production server should use
-                      // (gets superseded if process.env.PORT is specified at runtime)
+      // (gets superseded if process.env.PORT is specified at runtime)
 
       middlewares: [
         'render' // keep this as last one
@@ -166,7 +172,7 @@ export default defineConfig((/* ctx */) => {
       // extendPackageJson (json) {},
 
       // Electron preload scripts (if any) from /src-electron, WITHOUT file extension
-      preloadScripts: [ 'electron-preload' ],
+      preloadScripts: ['electron-preload'],
 
       // specify the debugging port to use for the Electron app when running in development mode
       inspectPort: 5858,
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,114 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page>
+    <q-table flat bordered title="Запросы" :rows="clientData" :columns="columns" row-key="name" >
+       <template v-slot:body-cell-action="props">
+        <q-td :props="props">
+          <div>
+            <q-btn outline  no-caps  icon="info"></q-btn>
+
+          </div>
+        </q-td>
+      </template>
+       <template v-slot:body-cell-method="props">
+        <q-td :props="props">
+          <div>
+            <q-badge :color="getMethodColor(props.val)" :label="props.value" />
+
+          </div>
+        </q-td>
+      </template>
+    </q-table>
   </q-page>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+
+const clientData = ref([])
+const columns = [
+  {
+    name: 'action',
+    label: '',
+  },
+  {
+    name: 'method',
+    label: 'Метод',
+    field: (row) => row.clientRequest.Method,
+    sortable: true,
+  },
+
+  {
+    name: 'url',
+    label: 'URL',
+    align: 'left',
+    field: (row) => row.clientRequest.URL,
+    sortable: true,
+  },
+  {
+    name: 'isErrorResponse',
+    label: 'Ошибка',
+    align: 'left',
+    field: (row) => (row.isErrorResponse ? 'Да' : 'Нет'),
+    sortable: true,
+  },
+  {
+    name: 'isProxy',
+    label: 'Перенаправлен?',
+    align: 'left',
+    field: (row) => (row.isProxy ? 'Да' : 'Нет'),
+    sortable: true,
+  },
+  {
+    name: 'proxyTo',
+    label: 'Перенаправлен на',
+    align: 'left',
+    field: (row) => {
+      if (row.isProxy) {
+        if (row.proxyTo.replaceByFakeRms) {
+          return row.proxyTo.fakeRms.url
+        }
+        return row.proxyTo.fakeRms.url
+      }
+      return row.mainRms.url
+    },
+    sortable: true,
+  },
+]
+const socket = new WebSocket('ws://127.0.0.1:9090/ws')
+function getMethodColor(val){
+  if(val == "GET"){
+    return 'green'
+  }
+  if(val == "POST"){
+    return 'yellod'
+  }
+  if(val == "PUT"){
+    return 'blue'
+  }
+  if(val == "DELETE"){
+    return 'red'
+  }
+  return 'orange'
+}
+
+socket.onopen = () => {
+  console.log('WebSocket connection established')
+}
+
+socket.onmessage = (event) => {
+  const data = JSON.parse(event.data)
+  clientData.value = clientData.value.concat(data)
+
+  console.log('Real-time update:', data)
+}
+
+socket.onerror = (error) => {
+  console.error('WebSocket error:', error)
+}
+
+socket.onclose = () => {
+  console.log('WebSocket connection closed')
+}
+
 //
 </script>
