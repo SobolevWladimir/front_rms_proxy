2ba99b5ba7195d9ad05d9c8a2b15bd6c1e432650
Vladimir <Sobolev.Wladim@yandex.ru>
2025-01-29 13:27:07 +0300

    Сделал первоначальную настройку

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,19 +1,55 @@
 <template>
   <q-page>
-    <q-table flat bordered title="Запросы" :rows="clientData" :columns="columns" row-key="name" >
-       <template v-slot:body-cell-action="props">
+    <input type="file" ref="file" style="display: none" @change="onChange" />
+    <q-dialog
+      v-model="showInfoDialog"
+      persistent
+      maximized
+      transition-show="slide-up"
+      transition-hide="slide-down"
+    >
+      <q-card class="">
+        <q-bar>
+          <q-space />
+
+          <q-btn dense flat icon="close" v-close-popup>
+            <q-tooltip class="bg-white text-primary">Close</q-tooltip>
+          </q-btn>
+        </q-bar>
+
+        <q-card-section>
+          <div class="text-h6">Запрос</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none ">
+          <textarea class="full-width" :value="modalData.clientRequest.Body"></textarea>
+        </q-card-section>
+
+        <q-card-section>
+          <div class="text-h6">Ответ</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none full-width row wrap justify-start items-start content-end">
+          <textarea class="full-width" :value="modalData.clientResponse.Body"></textarea>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
+    <div class="q-pa-sm">
+      <q-btn label="Экспорт данных" @click="exportData"></q-btn>
+      <q-btn label="Импорт данных" @click="$refs.file.click()"></q-btn>
+    </div>
+    <q-table flat bordered :rows="clientData" :columns="columns" row-key="name">
+      <template v-slot:body-cell-action="props">
         <q-td :props="props">
           <div>
-            <q-btn outline  no-caps  icon="info"></q-btn>
-
+            <q-btn outline no-caps icon="info" @click="showInfo(props.row)"></q-btn>
           </div>
         </q-td>
       </template>
-       <template v-slot:body-cell-method="props">
+      <template v-slot:body-cell-method="props">
         <q-td :props="props">
           <div>
             <q-badge :color="getMethodColor(props.val)" :label="props.value" />
-
           </div>
         </q-td>
       </template>
@@ -25,6 +61,8 @@
 import { ref } from 'vue'
 
 const clientData = ref([])
+const modalData = ref(null);
+const showInfoDialog = ref(false);
 const columns = [
   {
     name: 'action',
@@ -75,24 +113,26 @@ const columns = [
   },
 ]
 const socket = new WebSocket('ws://127.0.0.1:9090/ws')
-function getMethodColor(val){
-  if(val == "GET"){
+function getMethodColor(val) {
+  if (val == 'GET') {
     return 'green'
   }
-  if(val == "POST"){
+  if (val == 'POST') {
     return 'yellod'
   }
-  if(val == "PUT"){
+  if (val == 'PUT') {
     return 'blue'
   }
-  if(val == "DELETE"){
+  if (val == 'DELETE') {
     return 'red'
   }
   return 'orange'
 }
+const isConnected = ref(false)
 
 socket.onopen = () => {
   console.log('WebSocket connection established')
+  isConnected.value = true
 }
 
 socket.onmessage = (event) => {
@@ -108,6 +148,44 @@ socket.onerror = (error) => {
 
 socket.onclose = () => {
   console.log('WebSocket connection closed')
+  isConnected.value = false
+}
+
+function exportData() {
+  const filename = 'dump.json'
+  const text = JSON.stringify(clientData.value)
+
+  let element = document.createElement('a')
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text))
+  element.setAttribute('download', filename)
+  element.style.display = 'none'
+  document.body.appendChild(element)
+  element.click()
+  document.body.removeChild(element)
+}
+function onChange(e) {
+  let files = e.target.files
+  if (files.length == 0) {
+    return
+  }
+  let file = files[0]
+  let reader = new FileReader()
+
+  reader.readAsText(file)
+
+  reader.onload = function () {
+    let data = JSON.parse(reader.result)
+    clientData.value = data
+  }
+
+  reader.onerror = function () {
+    console.log(reader.error)
+  }
+}
+function showInfo(data){
+  modalData.value = data
+  console.log(modalData)
+  showInfoDialog.value = true;
 }
 
 //
